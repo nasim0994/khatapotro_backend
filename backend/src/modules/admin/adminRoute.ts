e0f8321cb1779e61e69705ccdc6f9f3a8a00b3cf
nasim0994 <nasim0994@gmail.com>
2026-06-16 16:21:50 +0600

    build slove

--- backend/src/modules/admin/adminRoute.ts
+++ backend/src/modules/admin/adminRoute.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import {
   addAdminController,
-  bulkBackAdminController,
   deleteAdminController,
   getAllAdminController,
   getSingleAdminController,
@@ -13,7 +12,6 @@ import {
 import verifyValidate from '../../middlewares/verifyValidate';
 import { adminValidation } from './adminValidation';
 import { verifyPermission } from '../../middlewares/verifyPermission';
-import { auth } from '../../middlewares/auth';
 const Router = express.Router();
 
 Router.post(
@@ -35,7 +33,7 @@ Router.put(
   '/update/password/by-superAdmin/:id',
   updatePasswordBySuperAdminController,
 );
-Router.put('/bulk-back', auth('superAdmin', 'Admin'), bulkBackAdminController);
+
 Router.delete(
   '/delete/:id',
   verifyPermission('admin', 'delete'),
